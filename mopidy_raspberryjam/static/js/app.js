b884de8d35f6e3d3b8751f481951acb51f2879fb
Ryan Babaie <yo.223@hotmail.com>
2015-08-04 17:49:13 -0400

    ye

--- mopidy_raspberryjam/static/js/app.js
+++ mopidy_raspberryjam/static/js/app.js
@@ -2,3 +2,7 @@ var raspberryjamApp = angular.module("raspberryjamApp", [
     'ngRoute',
     'raspberryjamControllers'
 ]);
+
+$(document).ready(function(){
+    $("#player-controls").height($("#player").height());
+});
--- mopidy_raspberryjam/static/js/controllers.js
+++ mopidy_raspberryjam/static/js/controllers.js
@@ -2,8 +2,8 @@ var raspberryControllers = angular.module('raspberryjamControllers', []);
 
 raspberryControllers.controller('QueueCtrl', function($scope){
 	$scope.queue = [
-		{title: "Somewhere in the Between", artist: "Streetlight Manifesto", runtime:254},
-		{title: "Damn", artist: "John Mackey", runtime: 326}
+		{title: "Somewhere in the Between", artist: "Streetlight Manifesto", runtime:254, genre: "Ska", img: "http://www.lyriki.com/images/thumb/4/40/AlbumArt-Streetlight_Manifesto-Somewhere_in_the_Between_%282007%29.jpg/250px-AlbumArt-Streetlight_Manifesto-Somewhere_in_the_Between_%282007%29.jpg"},
+		{title: "Damn", artist: "John Mackey", runtime: 326, genre: "Contemporary", img: "http://ostimusic.com/imagefiles/IMG_6496cropped.jpg"},
 	];
 
 
@@ -14,6 +14,12 @@ raspberryControllers.controller('QueueCtrl', function($scope){
 		var secs = Math.floor((seconds-hrs*3600-mins*60));
 
 		return str + (hrs > 0 ? hrs+ " hrs "  : "") + (mins > 0 ? mins + " min " : "") + (secs > 0 ? secs + " s" : "");
+	}
+
+	$scope.queueTimeTotal = function(){
+		var sum = 0;
+		$scope.queue.forEach(function(song,index,arr){sum += song.runtime});
+		return sum;
 	}
 });
 
