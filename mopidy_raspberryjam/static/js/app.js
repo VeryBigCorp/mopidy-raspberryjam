38d1bb5127c22f2af57780e7984752d9d1c6a976
Evan <fromhyper42@gmail.com>
2015-08-04 21:40:00 -0400

    sync dog with front end

--- mopidy_raspberryjam/static/js/app.js
+++ mopidy_raspberryjam/static/js/app.js
@@ -1,7 +1,30 @@
+function timeToStr(seconds) {
+  var str = "";
+  var hrs = Math.floor(seconds / 3600);
+  var mins = Math.floor((seconds - hrs*3600)/ 60);
+  var secs = Math.floor((seconds-hrs*3600-mins*60));
+
+  return str + (hrs > 0 ? bufferZeros(hrs) + ":"  : "") + (mins > 0 ? bufferZeros(mins) + ":" : "") + bufferZeros(secs);
+}
+
+function bufferZeros(amount) {
+  return ( amount < 10 ? "0" + amount : amount );
+}
+
 var raspberryjamApp = angular.module("raspberryjamApp", [
   'ngRoute',
   'raspberryjamControllers'
 ]);
+
+function populatePlayer() {
+  mopidy.playback.getCurrentTrack().done(function(track){
+    if(track){
+      $("#player-artist").html(track.artists.join(", "));
+      $("#player-title").html(track.name);
+      $("#player-length").html(timeToStr(track.length));
+    }
+  });
+}
 
 var mopidy;
 $(document).ready(function(){
