d3fad58e70dcccd83e78212965dc0a104e257c9c
root <root@stockpi.(none)>
2015-08-05 00:32:04 -0400

    Fixed player position update

--- mopidy_raspberryjam/static/js/app.js
+++ mopidy_raspberryjam/static/js/app.js
@@ -4,7 +4,7 @@ function timeToStr(seconds) {
   var mins = Math.floor((seconds - hrs*3600)/ 60);
   var secs = Math.floor((seconds-hrs*3600-mins*60));
 
-  return str + (hrs > 0 ? bufferZeros(hrs) + ":" : "") + (mins > 0 ? bufferZeros(mins) + ":" : "") + bufferZeros(secs);
+  return str + (hrs > 0 ? bufferZeros(hrs) + ":" : "") + (mins > 0 ? bufferZeros(mins) + ":" : "") + bufferZeros(secs) +  (seconds < 60 ? "s" : "");
 }
 
 function bufferZeros(amount) {
@@ -16,20 +16,45 @@ var raspberryjamApp = angular.module("raspberryjamApp", [
   'raspberryjamControllers'
 ]);
 
+var trackID;
+function togglePositionTrack(state, length){
+        var progress = $("#player-remaining");
+	clearInterval(trackID);
+	console.log("toggling track to "+state);
+	if(state){
+          progress.parent().show();
+
+          trackID = setInterval(function() {
+           mopidy.playback.getTimePosition().done(function(pos){
+              var remaining = length - pos;
+              progress.html(timeToStr( (remaining) / 1000 ) + " remaining...");
+              progress.attr("aria-valuenow", 100.0 * pos/length);
+              progress.css("width", 100.0 * (pos / length) + "%");
+	   });
+           }, 1000);
+	} else {
+	  progress.parent().hide();
+	  pos = 0;
+	}
+}
+
 function populatePlayer() {
   mopidy.playback.getCurrentTrack().done(function(track) {
     if(track){
       //do some fancy shit to get a list of the artist names
+      $("#player").show();
       $("#player-artist").html(track.artists.reduce(function(a, b){ return {name: a.name + ", " + b.name} }).name);
       $("#player-title").html(track.name);
       $("#player-length").html(timeToStr(track.length / 1000));
-      var remainingTime = track.length - mopidy.playback.getTimePosition();
-      $("#player-remaining").html(timeToStr( (remainingTime) / 1000 )).attr("aria-valuenow", 100.0 * remainingTime / track.length).css("width", 100.0 * remainingTime / track.length + "%");
+      $("#player-genre").html("");
+
+      togglePositionTrack(true, track.length);
     } else {
-      $("#player-artist").html("");
-      $("#player-title").html("Nothing playing...");
-      $("#player-length").html("");
-      $("#player-remaining").html("");
+      //$("#player-artist").html("");
+      //$("#player-title").html("Nothing playing...");
+      //$("#player-length").html("");
+      $("#player").hide();
+      togglePositionTrack(false, 0);
     }
   });
 }
@@ -41,14 +66,13 @@ $(document).ready(function() {
   //start mopidy
   mopidy = new Mopidy();
   
-  mopidy.on(console.log.bind(console));
-  
   mopidy.on("event:playbackStateChanged", function() {
     populatePlayer();
   });
   
   mopidy.on("state:online", function() {
     populatePlayer();
+    console.log("Online...");
   });
   
   $(window).unload( function() {
